64ab553a52d9068814682b8f5c30564a98ec7812
Elena <jelenreifs@gmail.com>
2021-01-18 09:00:54 +0100

    updata users y get reservas

--- users.js
+++ users.js
@@ -54,13 +54,76 @@ router.post("/add", function(req, res) {
             creditos : creditos,
             password : password,
             administrador : false
-        }, function (err, result) {
+        }, function (err, datos) {
      if(err !== null){
        res.send({error:true, mensaje: "Error al registrar el usuario"} )
      }else{
-       res.send({ error:false, mensaje: "Usuario registrado correctamente"})
+       res.send({ error:false, mensaje: "Usuario registrado correctamente", datos:datos })
      }
    })
+})
+
+
+
+/*******************************************************/
+/*          CONFIGURACION: MODIFICACION USER           */
+/*******************************************************/
+
+router.put("/update", function(req, res) {
+    let dni = req.body.dni;
+    let foto = req.body.foto;
+    let nombre = req.body.nombre;
+    let apellido = req.body.apellido;
+    let tfno = req.body.tfno;
+    let email = req.body.email;
+    let fechaAlta = req.body.fechaAlta;
+    let fechaBaja = req.body.fechaBaja;
+    let creditos = req.body.creditos;
+    let password = bcrypt.hashSync( req.body.password, 10 );
+    let administrador = req.body.administrador;
+
+  
+    let db = req.app.locals.db;
+  db.collection("users")
+    .find({
+      dni: dni,
+      foto: foto,
+      nombre: nombre,
+      apellido: apellido,
+      tfno: tfno,
+      email: email,
+      fechaAlta: fechaAlta,
+      fechaBaja: fechaBaja,
+      creditos: creditos,
+      password: password,
+      administrador: administrador
+    }).toArray((err, reserva) => {
+      if (err != null) {
+        res.send(err);
+      } else {
+        db.collection("users")
+          .updateMany({
+            dni: dni,
+            foto: foto,
+            nombre: nombre,
+            apellido: apellido,
+            tfno: tfno,
+            email: email,
+            fechaAlta: fechaAlta,
+            fechaBaja: fechaBaja,
+            creditos: creditos,
+            password: password,
+            administrador: administrador
+          }, function (err, datos) {
+              if (err != null) {
+                res.send(err);
+              } else { 
+                 res.send({ error:true, mensaje: "Los datos se han actualizado correctamente", datos:datos });
+              }
+           })
+       }
+     })
+
 })
 
 
